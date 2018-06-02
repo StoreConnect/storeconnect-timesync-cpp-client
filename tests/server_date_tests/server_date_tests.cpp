4666db224f2a68ac099b7a111a9efccb4dea473c
MGasztold <michal.gasztold@ubudu.com>
2018-06-02 21:53:34 +0200

    fix easyhttp_get instance

--- tests/server_date_tests/server_date_tests.cpp
+++ tests/server_date_tests/server_date_tests.cpp
@@ -13,14 +13,14 @@ TEST(basic_test, two_plus_two_test) {
 
 TEST(basic_test, basic_http_request) {
 
-    easyhttp_get http_get = *new easyhttp_get();
+    easyhttp_get *http_get = new easyhttp_get();
 
-    server_date server_date_instance("http://www.google.com/", 10, 5, &http_get);
+    server_date server_date_instance("http://www.google.com/", 10, 5, http_get);
     server_date_instance.offset_amortization_enabled(true);
     server_date_instance.auto_synchronize();
     long long server_now = server_date_instance.now();
     std::cout << "server_now: " << server_now << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    server_date_instance.stop_auto_synchronize();
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(5000));
+//    server_date_instance.stop_auto_synchronize();
+//    std::this_thread::sleep_for(std::chrono::seconds(10));
 }
